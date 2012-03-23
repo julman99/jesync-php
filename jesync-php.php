975373b3d0b4edede075f6f6684bfab2d4b6b53e
Julio Viera <julio.viera@gmail.com>
2012-03-23 19:06:55 -0400

    Cleaned-up autoload function

--- jesync-php.php
+++ jesync-php.php
@@ -3,20 +3,12 @@
 function _jesyncphp_autoload_funcs($class) {    
     if (substr($class, 0, strlen(__NAMESPACE__)) != __NAMESPACE__)
         return false;
-    //$class=substr($class, strlen("JESync"));
-    
-    $path = sprintf(
-            '%s/%s.php', __DIR__ , str_replace("\\", '/', $class)
-    );
-    
+    $path = $class.'.php';
     if (file_exists($path)){
         require_once($path);
     }else{
         return false;
     }
 }
-
-
-
 
 spl_autoload_register('_jesyncphp_autoload_funcs');
